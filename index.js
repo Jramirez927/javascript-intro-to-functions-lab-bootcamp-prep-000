66fd06a255d8d75320d75afaeca0582f51c5c545
Jorge Ramirez <jorgeramirez927@gmail.com>
2018-02-10 00:51:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,10 @@ function sayHiToGrandma(string){
   if (string === "I love you, Grandma."){
     return "I love you, too."
   }
-  else if (string === lowercase_str){
+  else if (string === uppercase_str){
+    return "YES INDEED!"
+  }
+  else {
     return  "I can\'t hear you!";
   }
-  else if( )
 }
